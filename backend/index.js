4f5032645cc776d85c0d38f6b2d8bd2611ab2b4f
Herique <heriquevitor3@gmail.com>
2022-09-05 07:21:21 -0300

    Database ON

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://heriquevitor:Lcnavi19031950@samsungmario2022.yd0q9ih.mongodb.net/test";
+
+const dbName = "jornada-fullstack-agosto";
+
+
+
+async function main() {
+console.log("Conectando ao banco de dados");
+const client = await MongoClient.connect(url);
+const db = client.db(dbName);
+const collection = db.collection("pontuacoes");
+console.log("Banco de dados conectado com sucesso");
+
 const app = express();
 
 app.get("/", function (req, res){
@@ -25,10 +40,20 @@ const lista = [
     id: 3,
     nome: "Bia",
     pontos: 400,
-  },
-];
-app.get("/pontuacoes", function(req, res) {
-  res.send("Ler todas as pontuações");
+  }];
+
+app.post("/pontuacoes", function(req, res){
+  const item = req.body;
+  lista.push({
+    id: lista.length + 1,
+    nome: item.nome,
+    pontos: item.pontos,
+  });
+  
+  res.send("Criado com sucesso");
 });
 
 app.listen(3000);
+}
+//Executando a função
+main();
